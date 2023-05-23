eb1942fba0e498cd4ee36558cb0d048fff5091d5
JbCantel <joaobcantel10@gmail.com>
2023-05-22 21:41:50 -0300

    lista

--- src/main/java/com/cantel/cadastro_de_trecos/Cadastro_de_trecos.java
+++ src/main/java/com/cantel/cadastro_de_trecos/Cadastro_de_trecos.java
@@ -4,21 +4,21 @@
  */
 package com.cantel.cadastro_de_trecos;
 
-
 import java.util.Scanner;
 
-
-/**
- *
- * @author 16128412023.1
- */
+// criando uma classe 'cadatro de trecos'
 public class Cadastro_de_trecos {
 
+// static : estático / void: diz o tipo / main: classe principal
     public static void main(String[] args) {
-        System.out.println("Cadatro de Trecos\n \n Menu: \n \t[1] Listar todos \n \t[2] Listar \n \t[3] Novo\n \t[4] Editar\n \t[5] Apagar\n \t[0] Sair");       
+        System.out.println("Cadatro de Trecos\n \n Menu: \n \t[1] Listar todos \n \t[2] Listar \n \t[3] Novo\n \t[4] Editar\n \t[5] Apagar\n \t[0] Sair");
         System.out.print("\nOpção: ");
-        
+
+        // scanner: objeto / New: criação de um novo objeto.
         Scanner scanner = new Scanner(System.in);
         String option = scanner.next();
+        if (option.length() != 1) {
+            System.out.println("errouuuuu!!");
+        }
     }
 }
