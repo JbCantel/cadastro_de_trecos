a0de852f7fbae8d834540706a41c87d299cac0b1
JbCantel <joaobcantel10@gmail.com>
2023-05-23 21:46:24 -0300

    Banco de dados mysql

--- src/main/java/com/cantel/cadastro_de_trecos/Cadastro_de_trecos.java
+++ src/main/java/com/cantel/cadastro_de_trecos/Cadastro_de_trecos.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 package com.cantel.cadastro_de_trecos;
 
 import java.util.Scanner;
@@ -9,16 +5,89 @@ import java.util.Scanner;
 // criando uma classe 'cadatro de trecos'
 public class Cadastro_de_trecos {
 
-// static : estático / void: diz o tipo / main: classe principal
-    public static void main(String[] args) {
-        System.out.println("Cadatro de Trecos\n \n Menu: \n \t[1] Listar todos \n \t[2] Listar \n \t[3] Novo\n \t[4] Editar\n \t[5] Apagar\n \t[0] Sair");
-        System.out.print("\nOpção: ");
+    /* scanner: objeto / New: criação de um novo objeto.
+    * static : podemos acessar diretamente no código
+     */
+    static Scanner scanner = new Scanner(System.in);
 
-        // scanner: objeto / New: criação de um novo objeto.
-        Scanner scanner = new Scanner(System.in);
+// static : estático / void: diz o tipo ( não retorna nada ) / main: classe principal
+    public static void main(String[] args) {
+         clearScreen();
+        mainMenu();
+
+    }
+
+    // Método que exibe o menu principal 
+    public static void mainMenu() {
+        System.out.println("Cadastro de Trecos\n \n Menu: \n \t[1] Listar todos \n \t[2] Listar \n \t[3] Novo\n \t[4] Editar\n \t[5] Apagar\n \t[0] Sair");
+        System.out.print("\nOpção: ");
+        // Recebe a opção do teclado.
         String option = scanner.next();
-        if (option.length() != 1) {
-            System.out.println("errouuuuu!!");
+
+        // Executa um método conforme a opção escolhida.
+        switch (option) {
+            case "0":
+                exitProgram();
+                break;
+            case "1":
+                listAll();
+                break;
+            case "2":
+                listOne();
+                break;
+            case "3":
+                newThing();
+                break;
+            case "4":
+                editThing();
+                break;
+            case "5":
+                deleteThing();
+                break;
+            default:
+                reloadMenu();
+        }
+    }
+
+    // Método que encerra o programa.
+    public static void exitProgram() {
+        scanner.close();
+        clearScreen();
+        System.out.println("\n\nHASTA LA VISTA!!\n\n");
+        System.exit(0);
+    }
+
+    // Lista todos os trecos cadastrados.
+    public static void listAll() {
+    }
+
+    // Lista um treco específico pelo Id
+    public static void listOne() {
+    }
+
+    // Cadastra um novo treco.
+    public static void newThing() {
+    }
+
+    // Edita um treco pelo Id.
+    public static void editThing() {
+    }
+
+    //Apaga um treco pelo Id.
+    public static void deleteThing() {
+    }
+    // Recarrega o menu principal.
+
+    public static void reloadMenu() {
+        clearScreen();
+        System.out.println("erroouuuuu!!");
+        mainMenu();
+    }
+
+    // Limpa a tela do terminal
+    public static void clearScreen() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("\n");
         }
     }
 }
---
+++ src/main/java/com/cantel/cadastro_de_trecos/connection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cantel.cadastro_de_trecos;
+
+/**
+ *
+ * @author 16128412023.1
+ */
+class connection {
+    
+}
